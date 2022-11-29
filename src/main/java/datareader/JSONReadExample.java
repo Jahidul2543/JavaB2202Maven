04ca590e33bd984eaed9a242f2288c26e0f0bb06
Jahidul Islam <jahidul@Jahiduls-MBP.lan>
2022-11-28 21:20:44 -0600

    Read JSON, Learn Map

--- src/main/java/datareader/JSONReadExample.java
+++ src/main/java/datareader/JSONReadExample.java
@@ -3,6 +3,7 @@ package datareader;
 // Java program to read JSON from a file
 
 import java.io.FileReader;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -14,21 +15,30 @@ public class JSONReadExample
 {
     public static void main(String[] args) throws Exception
     {
+        //01. Step One - Locate the file
+        FileReader fileReader = new FileReader("data/data.json");
+
+        // 02. Step 02 - Bring/convert JSON file information in a Java Object
         // parsing file "data.json"
-        Object obj = new JSONParser().parse(new FileReader("data.json"));
+        Object obj = new JSONParser().parse(fileReader);
+
 
         // typecasting obj to JSONObject
         JSONObject jo = (JSONObject) obj;
 
+        // 03. Step 03 - Take out information from the java object
         // getting firstName and lastName
         String firstName = (String) jo.get("firstName");
         String lastName = (String) jo.get("lastName");
 
+        // 04. Step 04 - Consume the information
         System.out.println(firstName);
         System.out.println(lastName);
 
+        // 03. Step 03 - Take out information from the java object
         // getting age
         long age = (long) jo.get("age");
+        // 04. Step 04 - Consume the information
         System.out.println(age);
 
         // getting address
@@ -36,6 +46,7 @@ public class JSONReadExample
 
         // iterating address Map
         Iterator<Map.Entry> itr1 = address.entrySet().iterator();
+
         while (itr1.hasNext()) {
             Map.Entry pair = itr1.next();
             System.out.println(pair.getKey() + " : " + pair.getValue());
@@ -44,7 +55,7 @@ public class JSONReadExample
         // getting phoneNumbers
         JSONArray ja = (JSONArray) jo.get("phoneNumbers");
 
-        // iterating phoneNumbers
+        // iterating phoneNumbers or a json object
         Iterator itr2 = ja.iterator();
 
         while (itr2.hasNext())
